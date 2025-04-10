7d552e3e3b87f3eb351f5a79be84d0f5f9a3dfee
Yoldoshev-Eldor <yoldosheveldor05@gmai.com>
2025-04-10 16:43:06 +0500

    Packages Dowload

--- SchoolManagement/SchoolManagement.DataAccess/Entities/Student.cs
+++ SchoolManagement/SchoolManagement.DataAccess/Entities/Student.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace SchoolManagement.DataAccess.Entities
+﻿namespace SchoolManagement.DataAccess.Entities
 {
     internal class Student
     {
