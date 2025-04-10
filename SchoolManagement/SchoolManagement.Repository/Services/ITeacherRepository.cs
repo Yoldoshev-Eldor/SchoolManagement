5e852d88276b59718ea31c6cc3dc860e52ee1310
Sherlock <shholmes359@gmail.com>
2025-04-10 21:37:24 +0500

    teacher repo added

---
+++ SchoolManagement/SchoolManagement.Repository/Services/ITeacherRepository.cs
@@ -0,0 +1,12 @@
+﻿using SchoolManagement.DataAccess.Entities;
+
+namespace SchoolManagement.Repository.Services;
+
+public interface ITeacherRepository
+{
+    Task<int> AddTeacherAsync(Teacher teacher);
+    Task<Teacher> GetTeacherByIdAsync(int teacherId);
+    Task<List<Teacher>> GetAllTeachersAsync(bool includeStudents = false, bool includeClasses = false);
+    Task UpdateTeacherAsync(Teacher teacher);
+    Task DeleteTeacherAsync(int teacherId);
+}
---
+++ SchoolManagement/SchoolManagement.Repository/Services/TeacherRepository.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SchoolManagement.DataAccess;
+using SchoolManagement.DataAccess.Entities;
+
+namespace SchoolManagement.Repository.Services;
+
+public class TeacherRepository : ITeacherRepository
+{
+    private readonly MainContext mainContext;
+    public TeacherRepository(MainContext context)
+    {
+        mainContext = context;
+    }
+
+    public async Task<int> AddTeacherAsync(Teacher teacher)
+    {
+        if (teacher == null)
+        {
+            throw new Exception("Teacher is null");
+        }
+        await mainContext.Teachers.AddAsync(teacher);
+        return await mainContext.SaveChangesAsync().ContinueWith(t => teacher.TeacherID);
+    }
+
+    public async Task DeleteTeacherAsync(int teacherId)
+    {
+        var teacher = mainContext.Teachers.FirstOrDefault(t => t.TeacherID == teacherId);
+        if (teacher == null)
+        {
+            throw new Exception("Teacher not found");
+        }
+        mainContext.Teachers.Remove(teacher);
+        await mainContext.SaveChangesAsync();
+    }
+
+    public async Task<List<Teacher>> GetAllTeachersAsync(bool includeStudents = false, bool includeClasses = false)
+    {
+        IQueryable<Teacher> query = mainContext.Teachers;
+        if (includeStudents)
+        {
+            query = query.Include(t => t.Students);
+        }
+        if (includeClasses)
+        {
+            query = query.Include(t => t.Classes);
+        }
+
+        return await query.ToListAsync();
+    }
+
+    public async Task<Teacher> GetTeacherByIdAsync(int teacherId)
+    {
+        var teacher = await mainContext.Teachers.FirstOrDefaultAsync(t => t.TeacherID == teacherId);
+        if (teacher == null)
+        {
+            throw new Exception("Teacher not found");
+        }
+        return teacher;
+    }
+
+    public async Task UpdateTeacherAsync(Teacher teacher)
+    {
+        var existingTeacher = mainContext.Teachers.FirstOrDefault(t => t.TeacherID == teacher.TeacherID);
+        if (existingTeacher == null)
+        {
+            throw new Exception("Teacher not found");
+        }
+        existingTeacher.FirstName = teacher.FirstName;
+        existingTeacher.LastName = teacher.LastName;
+        existingTeacher.Age = teacher.Age;
+        existingTeacher.Subject = teacher.Subject;
+        existingTeacher.Students = teacher.Students;
+        existingTeacher.Classes = teacher.Classes;
+        mainContext.Teachers.Update(existingTeacher);
+        await mainContext.SaveChangesAsync();
+    }
+}
