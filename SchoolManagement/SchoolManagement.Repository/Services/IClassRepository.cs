d38b33c55790f0d42b06fb99d153f74df191be56
Yoldoshev-Eldor <yoldosheveldor05@gmai.com>
2025-04-11 11:32:31 +0500

    Class Repo added

---
+++ SchoolManagement/SchoolManagement.Repository/Services/IClassRepository.cs
@@ -0,0 +1,12 @@
+﻿using SchoolManagement.DataAccess.Entities;
+
+namespace SchoolManagement.Repository.Services;
+
+public interface IClassRepository
+{
+    Task<long> AddClassAsync(Class addingClass);
+    Task DeleteClassAsync(long id);
+    Task<Class> GetClassByIdAsync(long id);
+    Task UpdateClassAsync(Class updatingClass);
+    Task<List<Class>> GetAllClass(bool studentClass,bool teacherClass,int skip,int take);
+}
